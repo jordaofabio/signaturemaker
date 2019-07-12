20a4477452e99c4484401c24a8a1e4f3509cabca
Fabio Jordão <fabiosjordao@gmail.com>
2019-07-12 00:25:40 -0300

    Ajustes

--- src/app/maker/maker.component.ts
+++ src/app/maker/maker.component.ts
@@ -32,6 +32,7 @@ export class MakerComponent implements OnInit {
       company: new FormControl(),
       menber_name: new FormControl()
     });
+    this.handleChange();
   }
 
   handleChange() {
